0802ffb9f10e57733bbbfac8fa8d84e7727be752
Andrey Krivonogov <andreykrv88@gmail.ru>
2019-10-19 12:01:55 +0300

    added Ray class. This is a vector parametric equation wich gives data about position on a ray depending from parameter t

--- RT in a week end/vec3.h
+++ RT in a week end/vec3.h
@@ -16,6 +16,7 @@ public:
 	vec3() {};
 
 	vec3(float e0, float e1, float e2) { e[0] = e0; e[1] = e1; e[2] = e2; }
+
 	inline float x() const { return e[0]; }
 	inline float y() const { return e[1]; }
 	inline float z() const { return e[2]; }
@@ -23,10 +24,10 @@ public:
 	inline float g() const { return e[1]; }
 	inline float b() const { return e[2]; }
 
-	inline const vec3& operator+() const       { return *this; }
-	inline vec3        operator-() const       { return vec3(-e[0], -e[1], -e[2]); }
-	inline float       operator[](ptrdiff_t i) const { return e[i]; }
-	inline float&      operator[](ptrdiff_t i)       { return e[i]; }
+	inline const vec3&       operator+()             const       { return *this; }
+	inline       vec3        operator-()             const       { return vec3(-e[0], -e[1], -e[2]); }
+	inline       float       operator[](ptrdiff_t i) const       { return e[i]; }
+	inline       float&      operator[](ptrdiff_t i)             { return e[i]; }
 
 	inline vec3& operator+=(const vec3& v2);
 	inline vec3& operator-=(const vec3& v2);
@@ -109,8 +110,8 @@ inline float dot(const vec3 &v1, const vec3 &v2)
 inline vec3 cross(const vec3& v1, const vec3& v2)
 {
 	return vec3((v1.e[1] * v2.e[2] - v1.e[2] * v2.e[1]),
-		(-(v1.e[0] * v2.e[2] - v1.e[2] * v2.e[0])),
-		(v1.e[0] * v2.e[1] - v1.e[1] * v2.e[0]));
+		      (-(v1.e[0] * v2.e[2] - v1.e[2] * v2.e[0])),
+		        (v1.e[0] * v2.e[1] - v1.e[1] * v2.e[0]));
 }
 
 inline vec3& vec3::operator+=(const vec3& v)
