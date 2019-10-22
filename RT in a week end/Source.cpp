5a462b03d2b3cdc637c68bcd0f1dd916f574ac29
Andrey Krivonogov <andreykrv88@gmail.ru>
2019-10-22 11:09:27 +0300

    Diffuse surface. When we have a hit at world, pick random point S that is tangent to the hitpoint and send a ray from hitpoint to S. After gettin results from Hit function, add results of these random ray

--- RT in a week end/Source.cpp
+++ RT in a week end/Source.cpp
@@ -13,7 +13,7 @@
 #include "Camera.h"
 #include <ctime>
 vec3 color(const ray& r, hitable* world);
-
+vec3 random_in_unit_sphere();
 inline double random_double() {
 	return rand() / (RAND_MAX + 1.0);
 }
@@ -53,7 +53,7 @@ int main()
 			}
 
 			col /= float(ns);
-			//col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
+			col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
 			int ir = int(255.99 * col[0]);
 			int ig = int(255.99 * col[1]);
 			int ib = int(255.99 * col[2]);
@@ -68,7 +68,8 @@ vec3 color(const ray& r, hitable *world)
 	hit_record rec;
 	if (world->hit(r, 0.0, FLT_MAX, rec))
 	{
-		return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+		vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+		return 0.5 * color(ray(rec.p, target - rec.p), world);
 	}
 
 	else
@@ -80,3 +81,12 @@ vec3 color(const ray& r, hitable *world)
 
 }
 
+vec3 random_in_unit_sphere()
+{
+	vec3 p;
+	do
+	{
+		p = 2.0 * vec3(random_double(), random_double(), random_double()) - vec3(1, 1, 1);
+	} while (p.squared_length() >= 1.0);
+	return p;
+}
